5a07b81c80ba5c8c8e28e5113dc84c91aff6210f
Leslie <leslie.appl@gmail.com>
2020-12-04 15:49:35 +0800

    debugged UI and saving data business logic

--- DocApp201202B/ViewController.swift
+++ DocApp201202B/ViewController.swift
@@ -30,8 +30,14 @@ class ViewController: UIViewController {
         set { self.doc!.people = newValue }
     }
     
+    @IBOutlet weak var statusBar: UILabel!
     @IBOutlet weak var listView: UITextView!
-    @IBOutlet weak var statusBar: UITextField!
+    @IBOutlet weak var displayDataView: UITextView!
+    
+    @IBOutlet weak var openFileTxtField: UITextField!
+    @IBOutlet weak var deleteFileTxtField: UITextField!
+    @IBOutlet weak var firstNameTxtField: UITextField!
+    @IBOutlet weak var lastNameTxtField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,6 +45,11 @@ class ViewController: UIViewController {
         let b = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(doAddFile(_:)))
         self.navigationItem.rightBarButtonItems = [b]
         self.title = "Group"
+        
+        openFileTxtField.bindToKeyboard()
+        deleteFileTxtField.bindToKeyboard()
+        firstNameTxtField.bindToKeyboard()
+        lastNameTxtField.bindToKeyboard()
         
         listFiles()
         
@@ -61,6 +72,70 @@ class ViewController: UIViewController {
     
     @IBAction func listRefreshBtnPressed(_ sender: UIButton) {
         listFiles()
+    }
+    
+    @IBAction func openFileBtnPressed(_ sender: UIButton) {
+        
+    }
+
+    @IBAction func addDataBtnPressed(_ sender: UIButton) {
+        
+        //TODO: Instructing add new data to a specified file url with an alert view.
+        let av = UIAlertController(title: "File Name", message: "Please enter file name for adding data.", preferredStyle: .alert)
+        av.addTextField(configurationHandler: {$0.autocapitalizationType = .words})
+        av.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        av.addAction(UIAlertAction(title: "OK", style: .default, handler: { (_) in
+            
+            //1 creating file name
+            guard let name = av.textFields?[0].text, !name.isEmpty else { return }
+            
+            //2 creating file url
+            self.fileURL = self.docsURL.appendingPathComponent(name).appendingPathExtension("pplgrp")
+            
+            guard let fileURL = self.fileURL else { return }
+            
+
+            // really should check to see if file by this name exists
+            if let _ = try? fileURL.checkResourceIsReachable() {
+    
+                //3 init uidocument object with url
+                self.doc = PeopleDocument(fileURL: self.fileURL!)
+
+                guard let firstName = self.firstNameTxtField.text else {return}
+                guard let lastName = self.lastNameTxtField.text else {return}
+                self.firstNameTxtField.text = ""
+                self.lastNameTxtField.text = ""
+                
+                //4 init Person object
+                let newP = Person(firstName: firstName, lastName: lastName)
+                
+                //5 adding Person to People Model in the UIDocument object
+                self.people.append(newP)
+                
+                //6 updating data changed
+                self.doc?.updateChangeCount(.done)
+                
+                self.statusBar.text = "New data added into: \(fileURL.lastPathComponent)"
+            }
+            else {
+                
+                //alerting file name duplicated
+                let av = UIAlertController(title: "File was not found", message: "Please create: \(fileURL.lastPathComponent) before adding data into it.", preferredStyle: .alert)
+                av.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                self.present(av, animated: true, completion: nil)
+            }
+
+        }))
+        self.present(av, animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func relaodDataBtnPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func deleteFileBtnPressed(_ sender: UIButton) {
+        
     }
 }
 
@@ -94,21 +169,21 @@ extension ViewController {
         self.statusBar.text = ""    //clean status bar first
         
         //1 Using alert view to create file name.
-        let av = UIAlertController(title: "New Group", message: "Enter name", preferredStyle: .alert)
+        let av = UIAlertController(title: "New File", message: "Enter name", preferredStyle: .alert)
         av.addTextField { (text) in
             text.autocapitalizationType = .words
         }
         av.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         av.addAction(UIAlertAction(title: "OK", style: .default, handler: { (_) in
             
-            guard let name = av.textFields![0].text, !name.isEmpty else {return}
+            guard let name = av.textFields?[0].text, !name.isEmpty else {return}
             
             //2 Creating File URL with the file name.
             self.fileURL = self.docsURL.appendingPathComponent((name as NSString).appendingPathExtension("pplgrp")!)
             
-            // really should check to see if file by this name exists
             guard let fileURL = self.fileURL else { return }
             
+            // really should check to see if file by this name exists
             if let _ = try? fileURL.checkResourceIsReachable() {
 
                 //alerting file name duplicated
@@ -170,5 +245,39 @@ extension ViewController {
         print("force saving")
     }
     
+    
+}
+
+//MARK: - Keyboard binding
+extension UIView {
+    // Any sub class of UIView can be binded to the Keyboard [ e.g UIButton.bindToKeyboard() ]
+    // Bound components have to be under the hierarchy of root 'UIView'.
+
+    func bindToKeyboard() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(_:)), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+    }
+    
+    @objc func keyboardWillChange(_ notification: NSNotification) {
+        let duration = notification.userInfo![UIResponder.keyboardAnimationDurationUserInfoKey] as! Double
+        let curve = notification.userInfo![UIResponder.keyboardAnimationCurveUserInfoKey] as! UInt
+
+    //Identifies the starting frame rectangle of the keyboard in screen coordinates.
+         let startingFrame = (notification.userInfo![UIResponder.keyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
+    //Identifies the ending frame rectangle of the keyboard in screen coordinates.
+         let endingFrame = (notification.userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+         let deltaY = endingFrame.origin.y - startingFrame.origin.y
+        
+        UIView.animateKeyframes(withDuration: duration, delay: 0.0, options: UIView.KeyframeAnimationOptions(rawValue: curve), animations: { self.frame.origin.y += deltaY }, completion: nil)
+    }
 
 }
+
+//MARK: - Keyboard dismissing
+extension ViewController {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        
+        view.endEditing(true)
+    }
+}
+
